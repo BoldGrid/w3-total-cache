8f4a03e7c3fc919a75acf5ef942595b7e4117ced
mshaposhnikov@w3-edge.com <mshaposhnikov@w3-edge.com>
2019-03-12 12:11:56 +0100

    make terms "accept" button url correct
    just names beautification

--- w3-total-cache-api.php
+++ w3-total-cache-api.php
@@ -22,7 +22,7 @@ define( 'W3TC_SUPPORT_REQUEST_URL', 'https://api.w3-edge.com/v1/support-request'
 define( 'W3TC_SUPPORT_SERVICES_URL', 'https://api.w3-edge.com/v1/support-serviceinventory' );
 define( 'W3TC_FAQ_URL', 'https://api.w3-edge.com/v1/redirects/faq' );
 define( 'W3TC_TERMS_URL', 'https://api.w3-edge.com/v1/redirects/policies-terms' );
-define( 'W3TC_TERMS_ACCEPT_URL', 'https://api.w3-edge.com/v1/redirects/policies-privacy' );
+define( 'W3TC_TERMS_ACCEPT_URL', 'https://api.w3-edge.com/v1/redirects/policies-accept' );
 define( 'W3TC_MAILLINGLIST_SIGNUP_URL', 'https://api.w3-edge.com/v1/signup-newsletter' );
 define( 'W3TC_NEWRELIC_SIGNUP_URL', 'https://api.w3-edge.com/v1/redirects/newrelic/signup' );
 define( 'W3TC_MAXCDN_SIGNUP_URL', 'https://api.w3-edge.com/v1/redirects/maxcdn/signup' );
