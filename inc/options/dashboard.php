88765a1b11185c930efa0323a93b02153d2bc8e9
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2020-07-08 14:36:55 -0400

    Add banner to dashboard. Fixes #223

--- inc/options/dashboard.php
+++ inc/options/dashboard.php
@@ -5,15 +5,55 @@ if ( !defined( 'W3TC' ) )
 	die();
 
 ?>
-<?php include W3TC_INC_DIR . '/options/common/header.php'; ?>
 
-<p>
-    <?php echo sprintf( __( 'The plugin is currently <span class="w3tc-%s">%s</span> in <strong>%s</strong> mode.', 'w3-total-cache' )
-	, $enabled ? "enabled" : "disabled"
-	, $enabled ? __( 'enabled', 'w3-total-cache' ) : __( 'disabled', 'w3-total-cache' )
-	, Util_Environment::w3tc_edition( $this->_config ) );
-?>
-</p>
+<div id="w3tc_dashboard_banner" class="metabox-holder">
+	<div class="postbox">
+		<div class="w3tc-postbox-ad">
+			<?php
+				echo wp_kses(
+					sprintf(
+						// Translators: 1 the opening anchor tag to the w3tc_support page, 2 its closing tag, 3 a line break.
+						__( 'Did you know that we offer premium support services?%3$s Our experts will configure W3 Total Cache for you! %1$sClick here for info%2$s.', 'w3-total-cache' ),
+						'<a href="' . admin_url( 'admin.php?page=w3tc_support' ) . '">',
+						'</a>',
+						'<br />'
+					),
+					array(
+						'a'  => array(
+							'href' => array()
+						),
+						'br' => array(),
+					)
+				);
+			?>
+		</div>
+		<h3 class="hndle">
+			<img style="height:32px;" src="<?php echo plugins_url( 'w3-total-cache/pub/img/W3TC_dashboard_logo_title.png' ); ?>" />
+		</h3>
+		<div class="inside">
+			<p>
+				<?php
+					echo wp_kses(
+						sprintf(
+							// Translators: 1 an opening strong tag, 2 its closing tag.
+							__( 'You\'re using the Community Edition of W3 Total Cache. Maximize your website\'s speed even more by upgrading to %1$sW3 Total Cache Pro%2$s to unlock advanced anaytics, fragment caching, full site delivery, extension support and other tools that will allow you to completely fine tune your website\'s performance.', 'w3-total-cache' ),
+							'<strong>',
+							'</strong>'
+						),
+						array( 'strong' => array() )
+					);
+				?>
+			</p>
+			<p>
+				<input
+					type="button"
+					class="button w3tc-gopro-button button-buy-plugin"
+					data-src="dashboard_banner" value="<?php esc_attr_e( 'Learn more about Pro', 'w3-total-cache' ) ?>" />
+			</p>
+		</div>
+	</div>
+</div>
+
 <form id="w3tc_dashboard" action="admin.php?page=<?php echo $this->_page; ?>" method="post">
     <p>
         Perform a
