aa0bcc4418c3bdb50a1254e1a2871cd3808ef7b8
Joe Cartonia <cssjoe@users.noreply.github.com>
2020-08-11 15:18:50 -0400

    Require files for request_filesystem_credentials() call.  Fixes #231 (#235)
    * Require files for request_filesystem_credentials() call.  Fixes #231.
    
    * Adding comments for #231
    
    Co-authored-by: bradm@inmotionhosting.com <bradm@inmotionhosting.com>

--- Util_WpFile.php
+++ Util_WpFile.php
@@ -240,6 +240,9 @@ class Util_WpFile {
 			$url = $_SERVER['REQUEST_URI'];
 		$url = preg_replace( "/&w3tc_note=([^&]+)/", '', $url );
 
+		// Ensure request_filesystem_credentials() is available.
+		require_once ABSPATH . 'wp-admin/includes/file.php';
+		require_once ABSPATH . 'wp-admin/includes/template.php';
 
 		$success = true;
 		ob_start();
@@ -281,6 +284,10 @@ class Util_WpFile {
 	 */
 	static private function get_filesystem_credentials_form( $method = '', $url = '',
 		$context = false ) {
+		// Ensure request_filesystem_credentials() is available.
+		require_once ABSPATH . 'wp-admin/includes/file.php';
+		require_once ABSPATH . 'wp-admin/includes/template.php';
+
 		ob_start();
 		// If first check failed try again and show error message
 		request_filesystem_credentials( $url, $method, true, false, array() );
