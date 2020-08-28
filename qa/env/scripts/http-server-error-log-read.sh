19ebd68c0fd42941e1885316053834590a080da8
mshaposhnikov@w3-edge.com <mshaposhnikov@w3-edge.com>
2020-08-28 13:39:10 +0200

    .

--- qa/env/scripts/http-server-error-log-read.sh
+++ qa/env/scripts/http-server-error-log-read.sh
@@ -13,7 +13,8 @@ cat $W3D_HTTP_SERVER_ERROR_LOG_FILENAME |\
   grep -v "wp-admin/includes/dashboard.php on line 1227" |\
   grep -v "WP_Community_Events::maybe_log_events_response" |\
   grep -v "wp-includes/Requests/Transport/cURL.php:422" |\
-  grep -v "Undefined index: recommended_version in /var/www/wp-sandbox/wp-admin/includes/class-wp-site-health.php"
+  grep -v "Undefined index: recommended_version in /var/www/wp-sandbox/wp-admin/includes/class-wp-site-health.php" |\
+  grep -v "Undefined index: no_update in /var/www/wp-sandbox/wp-includes/update.php"
 
 if [ -f "${W3D_WP_CONTENT_PATH}debug.log" ]; then
 	cat "${W3D_WP_CONTENT_PATH}debug.log" |\
@@ -29,7 +30,8 @@ if [ -f "${W3D_WP_CONTENT_PATH}debug.log" ]; then
 	grep -v "wp-admin/includes/dashboard.php on line 1227" |\
 	grep -v "WP_Community_Events::maybe_log_events_response" |\
 	grep -v "wp-includes/Requests/Transport/cURL.php:422" |\
-    grep -v "Undefined index: recommended_version in /var/www/wp-sandbox/wp-admin/includes/class-wp-site-health.php"
+    grep -v "Undefined index: recommended_version in /var/www/wp-sandbox/wp-admin/includes/class-wp-site-health.php" |\
+    grep -v "Undefined index: no_update in /var/www/wp-sandbox/wp-includes/update.php"
 fi
 # esc_attr in eval - pagecache/late-init test
 # in php7.0-fpm call stack is printed so should be eliminated
