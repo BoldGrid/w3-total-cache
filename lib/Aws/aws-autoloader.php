e02d65262e1ef67931faf61ef4c088e2cd30d931
Joe Cartonia <joec@boldgrid.com>
2020-09-04 13:04:44 -0400

    Check for Aws before loading functions.  Fixes #247

--- lib/Aws/aws-autoloader.php
+++ lib/Aws/aws-autoloader.php
@@ -1159,7 +1159,10 @@ spl_autoload_register(function ($class) use ($mapping) {
     }
 }, true);
 
-require __DIR__ . '/Aws/functions.php';
+if ( ! function_exists( 'Aws\parse_ini_file' ) ) {
+	require __DIR__ . '/Aws/functions.php';
+}
+
 require __DIR__ . '/GuzzleHttp/functions_include.php';
 require __DIR__ . '/GuzzleHttp/Psr7/functions_include.php';
 require __DIR__ . '/GuzzleHttp/Promise/functions_include.php';
