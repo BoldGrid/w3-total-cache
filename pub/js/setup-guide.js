87b8a5a9ba8d4c0de7d2d881cff55949dce8fcf5
Joe Cartonia <joec@boldgrid.com>
2020-12-21 16:25:10 -0500

    Fix setup compelte slide for TTFB and Page Cache info

--- pub/js/setup-guide.js
+++ pub/js/setup-guide.js
@@ -1216,8 +1216,11 @@ function w3tc_wizard_actions( $slide ) {
 
 		case 'w3tc-wizard-slide-complete':
 			var html,
-				pgcacheEngine = $container.find( 'input:checked[name="pgcache_engine"]' ).val();
-				pgcacheDiffPercent = $container.find( '#test-results' ).data( 'pgcacheDiffPercent-' + pgcacheEngine ),
+				pgcacheEngine = $container.find( 'input:checked[name="pgcache_engine"]' ).val(),
+				pgcacheEngineLabel = $container.find( 'input:checked[name="pgcache_engine"]' )
+					.closest('td').next('td').text(),
+				pgcacheDiffPercent = $container.find( '#test-results' )
+					.data( 'pgcacheDiffPercent-' + pgcacheEngine ),
 				dbcacheEngine = $container.find( 'input:checked[name="dbcache_engine"]' ).val(),
 				dbcacheEngineLabel = $container.find( 'input:checked[name="dbcache_engine"]' )
 					.closest('td').next('td').text(),
@@ -1245,11 +1248,14 @@ function w3tc_wizard_actions( $slide ) {
 			$container.find( '#w3tc-options-menu li' ).removeClass( 'is-active' );
 			$container.find( '#w3tc-options-menu li' ).last().addClass( 'is-active' );
 
-			html = ( pgcacheDiffPercent > 0 ? '+' : '' ) +
-				pgcacheDiffPercent +
-				'%';
+			html = pgcacheDiffPercent !== undefined ?
+				( pgcacheDiffPercent > 0 ? '+' : '' ) +
+				parseFloat( pgcacheDiffPercent ).toFixed( 2 ) +
+				'%' : '0.00%';
 
 			$container.find( '#w3tc-ttfb-diff' ).html( html );
+
+			$container.find( '#w3tc-pgcache-engine' ).html( pgcacheEngineLabel );
 
 			$container.find( '#w3tc-dbcache-engine' ).html( dbcacheEngineLabel );
 
