ce53430bd41282e3bf07cfb9a9d23d879fee340f
Joe Cartonia <joec@boldgrid.com>
2021-01-13 10:54:41 -0500

    Add counter for top level

--- pub/js/feature-counter.js
+++ pub/js/feature-counter.js
@@ -9,15 +9,21 @@
  */
 
 jQuery(function() {
-	var $menuItem = jQuery( '#toplevel_page_w3tc_dashboard a[href="admin.php?page=w3tc_feature_showcase"]' );
-
-	if ( W3TCFeatureShowcaseData.unseenCount && $menuItem.length ) {
-		$menuItem.append(
-			' <span class="awaiting-mod count-' +
+	var $menuItem = jQuery( '#toplevel_page_w3tc_dashboard.wp-not-current-submenu a[href="admin.php?page=w3tc_dashboard"] .wp-menu-name' ),
+		$submenuItem = jQuery( '#toplevel_page_w3tc_dashboard a[href="admin.php?page=w3tc_feature_showcase"]' ),
+		markup = ' <span class="awaiting-mod count-' +
 			W3TCFeatureShowcaseData.unseenCount +
 			'"><span class="feature-count">' +
 			W3TCFeatureShowcaseData.unseenCount +
-			'</span></span>'
-		);
+			'</span></span>';
+
+	if ( W3TCFeatureShowcaseData.unseenCount ) {
+		if ( $menuItem.length ) {
+			$menuItem.append( markup );
+		}
+
+		if ( $submenuItem.length ) {
+			$submenuItem.append( markup );
+		}
 	}
 });
