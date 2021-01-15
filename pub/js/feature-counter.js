c7cfa598db6345e7fc729aa9f68e60c661bb5624
Joe Cartonia <joec@boldgrid.com>
2021-01-15 13:47:16 -0500

    Update menu items

--- pub/js/feature-counter.js
+++ pub/js/feature-counter.js
@@ -9,7 +9,8 @@
  */
 
 jQuery(function() {
-	var $menuItem = jQuery( '#toplevel_page_w3tc_dashboard.wp-not-current-submenu a[href="admin.php?page=w3tc_dashboard"] .wp-menu-name' ),
+	var $adminmenuItem = jQuery ( '#wp-admin-bar-w3tc_feature_showcase a' ),
+		$menuItem = jQuery( '#toplevel_page_w3tc_dashboard.wp-not-current-submenu a[href="admin.php?page=w3tc_dashboard"] .wp-menu-name' ),
 		$submenuItem = jQuery( '#toplevel_page_w3tc_dashboard a[href="admin.php?page=w3tc_feature_showcase"]' ),
 		markup = ' <span class="awaiting-mod count-' +
 			W3TCFeatureShowcaseData.unseenCount +
@@ -17,7 +18,23 @@ jQuery(function() {
 			W3TCFeatureShowcaseData.unseenCount +
 			'</span></span>';
 
-	if ( W3TCFeatureShowcaseData.unseenCount ) {
+	if ( W3TCFeatureShowcaseData.unseenCount > 0 ) {
+		if ( $adminmenuItem.length ) {
+			$adminmenuItem.append( markup );
+
+			$adminmenuItem.find( '.awaiting-mod' ).css(
+				{
+					padding: "0 5px",
+					"min-width": "18px",
+					height: "18px",
+					"border-radius": "9px",
+					"background-color": "#ca4a1f",
+					color: "#fff",
+					"font-size": "11px"
+				}
+			);
+		}
+
 		if ( $menuItem.length ) {
 			$menuItem.append( markup );
 		}
