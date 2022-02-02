016dc6891d9948d5fb9368f43bfa245cbd9282a8
Joe Cartonia <joec@boldgrid.com>
2022-02-02 15:09:53 -0500

    Print more info for WP setup

--- qa/env/scripts/init-box/700-wordpress.sh
+++ qa/env/scripts/init-box/700-wordpress.sh
@@ -1,13 +1,18 @@
 URL="${W3D_HTTP_SERVER_SCHEME}://${W3D_WP_HOST}${W3D_WP_MAYBE_COLON_PORT}${W3D_WP_SITE_URI}"
 LIMITED="sudo -u www-data"
 
-mkdir -pv ${W3D_WP_PATH}
-curl -L --silent --show-error http://wordpress.org/wordpress-${W3D_WP_VERSION}.tar.gz --output ${W3D_WP_PATH}wordpress.tar.gz
+echo "Installing WordPress ${W3D_WP_VERSION} at ${W3D_WP_PATH} for ${W3D_HTTP_SERVER_SCHEME}://${W3D_WP_HOST}${W3D_WP_MAYBE_COLON_PORT}${W3D_WP_SITE_URI} ..."
 
-cd ${W3D_WP_PATH}
+mkdir -pv $W3D_WP_PATH
 
-tar xzf ${W3D_WP_PATH}wordpress.tar.gz --strip-components=1
-chown -R www-data:www-data ${W3D_WP_PATH}
+cd $W3D_WP_PATH
+
+$LIMITED wp core download --version=$W3D_WP_VERSION
+
+echo -n "Installed.  Checking WordPress version... "
+$LIMITED wp core version
+
+echo "Creating database and user..."
 
 mysql -uroot <<SQL
 CREATE DATABASE wordpress;
@@ -17,6 +22,8 @@ GRANT ALL PRIVILEGES ON wordpress.* TO wordpress@localhost;
 FLUSH PRIVILEGES;
 SQL
 
+echo "Configuring WordPress..."
+
 $LIMITED wp core config --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --extra-php <<PHP
 define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_LOG', true );
@@ -24,6 +31,8 @@ PHP
 
 # Remove default WP_DEBUG false definition
 sed -i "/'WP_DEBUG', false/d" wp-config.php
+
+echo "Completing WordPress setup..."
 
 $LIMITED wp core install --url=$URL --title=sandbox --admin_user=admin --admin_password=1 --admin_email=a@b.com --skip-email
 # set predefined time format (expected by QA changing post's time)
@@ -41,19 +50,26 @@ chown www-data:www-data ./disable-wp-updates.php
 
 sed -i '/require_wp_db();/a require( ABSPATH . \"/disable-wp-updates.php\" );   //w3tc test' wp-settings.php
 
+echo "Change permalink structure..."
+
 # change url structure
 $LIMITED wp rewrite structure '/%year%/%monthnum%/%day%/%postname%/'
 $LIMITED wp rewrite flush --hard
 
 # extras
 if [ "$W3D_WP_NETWORK" = "subdomain" ] || [ "$W3D_WP_NETWORK" = "subdir" ]; then
-	/share/scripts/init-box/720-wordpress-network.sh
+    echo "Configuring WordPress network for ${W3D_WP_NETWORK}..."
+    /share/scripts/init-box/720-wordpress-network.sh
 fi
 
 if [ "$W3D_HTTP_SERVER" = "apache" ]; then
-	/share/scripts/init-box/750-wordpress-apache-htaccess.sh
+    echo "Configuring WordPress for use with Apache..."
+    /share/scripts/init-box/750-wordpress-apache-htaccess.sh
 fi
 
 if [ "$W3D_WP_CONTENT_PATH" != "${W3D_WP_PATH}wp-content/" ]; then
-	/share/scripts/init-box/760-wordpress-pathmoved.sh
+    echo "Configuring WordPress with an alternate content path: ${W3D_WP_CONTENT_PATH}"
+    /share/scripts/init-box/760-wordpress-pathmoved.sh
 fi
+
+echo "WordPress setup complete."
