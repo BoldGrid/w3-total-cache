42c954d5f2a2cf8207d4b2a4849ac3cdf18d2570
Joe Cartonia <joec@boldgrid.com>
2022-02-03 14:31:34 -0500

    Fix WordPress setup script

--- qa/env/scripts/init-box/700-wordpress.sh
+++ qa/env/scripts/init-box/700-wordpress.sh
@@ -32,14 +32,15 @@ PHP
 # Remove default WP_DEBUG false definition
 sed -i "/'WP_DEBUG', false/d" wp-config.php
 
+# disable any automatic updates
+echo "Disabling automatic updater..."
+sed -i '2idefine( \"AUTOMATIC_UPDATER_DISABLED\", true );' wp-config.php
+
 echo "Completing WordPress setup..."
 
 $LIMITED wp core install --url=$URL --title=sandbox --admin_user=admin --admin_password=1 --admin_email=a@b.com --skip-email
 # set predefined time format (expected by QA changing post's time)
 $LIMITED wp option set time_format "H:i"
-
-# disable any automatic updates
-sed -i '2idefine( \"AUTOMATIC_UPDATER_DISABLED\", true );' wp-config.php
 
 # add header mark showing PHP was executed
 sed -i '2iheader( \"w3tc_php: executed\" );' wp-config.php
@@ -50,10 +51,11 @@ chown www-data:www-data ./disable-wp-updates.php
 
 sed -i '/require_wp_db();/a require( ABSPATH . \"/disable-wp-updates.php\" );   //w3tc test' wp-settings.php
 
-echo "Change permalink structure..."
-
 # change url structure
+echo "Change permalink structure..."
 $LIMITED wp rewrite structure '/%year%/%monthnum%/%day%/%postname%/'
+
+echo "Flushing rewrites hard..."
 $LIMITED wp rewrite flush --hard
 
 # extras
