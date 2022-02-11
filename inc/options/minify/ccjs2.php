520eb6706d19384441be7fa23dc5d1b4b8f7b2fb
jacobd91 <jacobd@inmotionhosting.com>
2022-02-11 09:58:23 -0500

    Initial commit for escaping output fixes

--- inc/options/minify/ccjs2.php
+++ inc/options/minify/ccjs2.php
@@ -24,7 +24,7 @@ $compilation_level = $c->get_string( 'minify.ccjs.options.compilation_level' );
 		<input id="minify__ccjs__path__java" class="js_enabled" type="text"
 			<?php Util_Ui::sealing_disabled( 'minify.' ) ?>
 			name="minify__ccjs__path__java"
-			value="<?php echo esc_attr( $c->get_string( 'minify.ccjs.path.java' ) ) ?>"
+			value="<?php esc_attr_e( $c->get_string( 'minify.ccjs.path.java' ), 'w3-total-cache' ) ?>"
 			size="60" />
 	</td>
 </tr>
@@ -38,16 +38,16 @@ $compilation_level = $c->get_string( 'minify.ccjs.options.compilation_level' );
 		<input id="minify__ccjs__path__jar" class="js_enabled" type="text"
 			<?php Util_Ui::sealing_disabled( 'minify.' ) ?>
 			name="minify__ccjs__path__jar"
-			value="<?php echo esc_attr( $c->get_string( 'minify.ccjs.path.jar' ) ) ?>"
+			value="<?php esc_attr_e( $c->get_string( 'minify.ccjs.path.jar' ), 'w3-total-cache' ) ?>"
 			size="60" />
 	</td>
 </tr>
 <tr>
 	<th>&nbsp;</th>
 	<td>
-		<input class="minifier_test js_enabled button {type: 'ccjs', nonce: '<?php echo wp_create_nonce( 'w3tc' ) ?>'}"
+		<input class="minifier_test js_enabled button {type: 'ccjs', nonce: '<?php esc_attr_e( wp_create_nonce( 'w3tc' ), 'w3-total-cache' ); ?>'}"
 			type="button"
-			value="<?php _e( 'Test Closure Compiler', 'w3-total-cache' ) ?>" />
+			value="<?php esc_attr_e( 'Test Closure Compiler', 'w3-total-cache' ) ?>" />
 		<span class="minifier_test_status w3tc-status w3tc-process"></span>
 	</td>
 </tr>
@@ -62,9 +62,9 @@ $compilation_level = $c->get_string( 'minify.ccjs.options.compilation_level' );
 			name="minify__ccjs__options__compilation_level"
 			<?php Util_Ui::sealing_disabled( 'minify.' ) ?>>
 			<?php foreach ( $compilation_levels as $compilation_level_key => $compilation_level_name ): ?>
-				<option value="<?php echo esc_attr( $compilation_level_key ); ?>"
+				<option value="<?php esc_attr_e( $compilation_level_key, 'w3-total-cache' ); ?>"
 					<?php selected( $compilation_level, $compilation_level_key ) ?>>
-					<?php echo $compilation_level_name; ?>
+					<?php esc_html_e( $compilation_level_name, 'w3-total-cache' ); ?>
 				</option>
 			<?php endforeach ?>
 		</select>
