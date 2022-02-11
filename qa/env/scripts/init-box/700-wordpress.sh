60e96b1ff53d576d50feb770d65714adbf37cb70
mshaposhnikov@w3-edge.com <mshaposhnikov@w3-edge.com>
2022-02-11 16:54:26 +0100

    more stable wp-updates blocker

--- qa/env/scripts/init-box/700-wordpress.sh
+++ qa/env/scripts/init-box/700-wordpress.sh
@@ -23,56 +23,49 @@ GRANT ALL PRIVILEGES ON wordpress.* TO wordpress@localhost;
 FLUSH PRIVILEGES;
 SQL
 
-echo "Configuring WordPress..."
-
-$LIMITED wp core config --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --extra-php <<PHP
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-PHP
-
-# Remove default WP_DEBUG false definition
-sed -i "/'WP_DEBUG', false/d" wp-config.php
-
-# disable any automatic updates
 echo "Disabling automatic updater..."
-sed -i '2idefine( \"AUTOMATIC_UPDATER_DISABLED\", true );' wp-config.php
-
-echo "Completing WordPress setup..."
-
-$LIMITED wp core install --url=$URL --title=sandbox --admin_user=admin --admin_password=1 --admin_email=a@b.com --skip-email
-# set predefined time format (expected by QA changing post's time)
-$LIMITED wp option set time_format "H:i"
-
-# add header mark showing PHP was executed
-sed -i '2iheader( \"w3tc_php: executed\" );' wp-config.php
-
 # disable all api calls to check for updates
 cp /share/scripts/init-box/templates/disable-wp-updates.php ./disable-wp-updates.php
 chown www-data:www-data ./disable-wp-updates.php
 
 sed -i '/require_wp_db();/a require( ABSPATH . \"/disable-wp-updates.php\" );   //w3tc test' wp-settings.php
 
+echo "Configuring WordPress..."
+$LIMITED wp core config --dbname=wordpress --dbuser=wordpress --dbpass=wordpress
+$LIMITED wp config set WP_AUTO_UPDATE_CORE false
+$LIMITED wp config set AUTOMATIC_UPDATER_DISABLED true
+$LIMITED wp config set WP_DEBUG true
+$LIMITED wp config set WP_DEBUG_LOG true
+
+cd $W3D_WP_PATH
+$LIMITED wp core install --url=$URL --title=sandbox --admin_user=admin --admin_password=1 --admin_email=a@b.com
+
+# set predefined time format (expected by QA changing post's time)
+$LIMITED wp option set time_format "H:i"
+
+# add header mark showing PHP was executed
+sed -i '2iheader( \"w3tc_php: executed\" );' wp-config.php
+
 # change url structure
 echo "Change permalink structure..."
 $LIMITED wp rewrite structure '/%year%/%monthnum%/%day%/%postname%/'
-
 echo "Flushing rewrites hard..."
 $LIMITED wp rewrite flush --hard
 
 # extras
 if [ "$W3D_WP_NETWORK" = "subdomain" ] || [ "$W3D_WP_NETWORK" = "subdir" ]; then
-    echo "Configuring WordPress network for ${W3D_WP_NETWORK}..."
-    /share/scripts/init-box/720-wordpress-network.sh
+	echo "Configuring WordPress network for ${W3D_WP_NETWORK}..."
+	/share/scripts/init-box/720-wordpress-network.sh
 fi
 
 if [ "$W3D_HTTP_SERVER" = "apache" ]; then
-    echo "Configuring WordPress for use with Apache..."
-    /share/scripts/init-box/750-wordpress-apache-htaccess.sh
+	echo "Configuring WordPress for use with Apache..."
+	/share/scripts/init-box/750-wordpress-apache-htaccess.sh
 fi
 
 if [ "$W3D_WP_CONTENT_PATH" != "${W3D_WP_PATH}wp-content/" ]; then
-    echo "Configuring WordPress with an alternate content path: ${W3D_WP_CONTENT_PATH}"
-    /share/scripts/init-box/760-wordpress-pathmoved.sh
+	echo "Configuring WordPress with an alternate content path: ${W3D_WP_CONTENT_PATH}"
+	/share/scripts/init-box/760-wordpress-pathmoved.sh
 fi
 
 echo "WordPress setup complete."
