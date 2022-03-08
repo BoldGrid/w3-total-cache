33a2f6be3132a7a18afa3e29cb17842a51e244f4
Richard Vodden <richard.vodden@babylonhealth.com>
2022-03-08 18:15:46 +0000

    Fix/#461 - Add a 'public_objects' option to the CloudFront CDN and remove the ACL when it is set (#462)
    * CDN: Cloufront: Ensure that _get_origin works for regions outside of eu-west-1
    * CDN: CloudFront: Add the 'public_objects' option
    * CDN: CloudFront: Ensure that 'public-read' ACL is not set when 'public_objects' is disabled
    * Fix exception call

--- CdnEngine_CloudFront.php
+++ CdnEngine_CloudFront.php
@@ -147,7 +147,12 @@ class CdnEngine_CloudFront extends CdnEngine_Base {
 	 * Returns origin
 	 */
 	function _get_origin() {
-		return sprintf( '%s.s3.amazonaws.com', $this->_config['bucket'] );
+		if ( $this->_config['bucket_location'] === 'us-east-1' ) {
+			$region = "";
+		} else {
+			$region = $this->_config['bucket_location'] . '.';
+		}
+		return sprintf( '%s.s3.%samazonaws.com', $this->_config['bucket'], $region );
 	}
 
 	/**
