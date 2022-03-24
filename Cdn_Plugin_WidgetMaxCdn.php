b42487368562148535ff7dd54492fcd5bf00642d
jacobd91 <jacobd@inmotionhosting.com>
2022-03-24 12:37:46 -0400

    Review feedback tweaks implemented

--- Cdn_Plugin_WidgetMaxCdn.php
+++ Cdn_Plugin_WidgetMaxCdn.php
@@ -38,12 +38,6 @@ class Cdn_Plugin_WidgetMaxCdn {
 
 		add_action( 'w3tc_ajax_cdn_maxcdn_widgetdata', array(
 			$this, 'w3tc_ajax_cdn_maxcdn_widgetdata' ) );
-
-		$page_val = Util_Request::get_string( 'page' );
-		if ( $this->have_zone && $this->authorized && ! empty( $page_val ) &&
-				strpos( $page_val, 'w3tc_dashboard' ) !== false ) {
-
-		}
 	}
 
 	function w3tc_ajax_cdn_maxcdn_widgetdata() {
