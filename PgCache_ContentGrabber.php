604469eb227137d19fff09e713e3e29b93f584f6
mshaposhnikov@w3-edge.com <mshaposhnikov@w3-edge.com>
2022-05-04 17:17:39 +0200

    add separate entry for bla and bla/ pages
    so that 301 redirects may be issued by wp for variants with/without
    slash. that makes google happier

--- PgCache_ContentGrabber.php
+++ PgCache_ContentGrabber.php
@@ -127,6 +127,7 @@ class PgCache_ContentGrabber {
 
 		$this->_request_uri = isset( $_SERVER['REQUEST_URI'] ) ?
 			filter_var( stripslashes( $_SERVER['REQUEST_URI'] ), FILTER_SANITIZE_URL ) : ''; // phpcs:ignore
+
 		$this->_lifetime = $this->_config->get_integer( 'pgcache.lifetime' );
 		$this->_late_init = $this->_config->get_boolean( 'pgcache.late_init' );
 		$this->_late_caching = $this->_config->get_boolean( 'pgcache.late_caching' );
@@ -1540,6 +1541,8 @@ class PgCache_ContentGrabber {
 
 		if ( empty( $page_key_extension['group'] ) ) {
 			if ( $this->_enhanced_mode || $this->_nginx_memcached ) {
+				$extra = '';
+
 				// URL decode
 				$key = urldecode( $key );
 
@@ -1553,13 +1556,16 @@ class PgCache_ContentGrabber {
 				$key = preg_replace( '~\?.*$~', '', $key );
 
 				// make sure one slash is at the end
+				if (substr($key, strlen($key) - 1, 1) == '/') {
+					$extra = '_slash';
+				}
 				$key = trim( $key, '/' ) . '/';
 
 				if ( $this->_nginx_memcached ) {
 					return $key;
 				}
 
-				return $key . '_index';
+				return $key . '_index' . $extra;
 			}
 		}
 
