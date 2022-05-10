4a91f8a2458d8734a405a99be027e28ed7c39f84
Joe Cartonia <joec@boldgrid.com>
2022-05-10 11:31:32 -0400

    Fix DbCache error

--- DbCache_WpdbInjection_QueryCaching.php
+++ DbCache_WpdbInjection_QueryCaching.php
@@ -539,7 +539,8 @@ class DbCache_WpdbInjection_QueryCaching extends DbCache_WpdbInjection {
 			'wp-register',
 		);
 
-		$request_uri = isset( $_SERVER['REQUEST_URI'] ) ? sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) ) : '';
+		$request_uri = isset( $_SERVER['REQUEST_URI'] ) ? filter_var( wp_unslash( $_SERVER['REQUEST_URI'] ), FILTER_SANITIZE_URL ) : '';
+
 		foreach ( $auto_reject_uri as $uri ) {
 			if ( strstr( $request_uri, $uri ) !== false ) {
 				return false;
