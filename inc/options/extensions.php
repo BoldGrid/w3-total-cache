0012e8f235e8f64336bd138ad5e4205bece35918
Joe Cartonia <joec@boldgrid.com>
2022-07-01 13:28:05 -0400

    Fix extension in settings URL.  Fixes #528

--- inc/options/extensions.php
+++ inc/options/extensions.php
@@ -28,7 +28,7 @@ if ( ! defined( 'W3TC' ) ) {
 		);
 	}
 	?>
-	<form action="admin.php?page=<?php echo esc_attr( $this->_page ); ?><?php echo $extension ? '&extension={' . esc_url( $extension ) . '}&action=view' : ''; ?>" method="post">
+	<form action="admin.php?page=<?php echo esc_attr( $this->_page ); ?><?php echo $extension ? '&extension=' . esc_attr( $extension ) . '&action=view' : ''; ?>" method="post">
 		<div class="metabox-holder <?php echo $extension ? 'extension-settings' : ''; ?>">
 			<?php require W3TC_INC_OPTIONS_DIR . "/extensions/$sub_view.php"; ?>
 		</div>
