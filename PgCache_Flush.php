0f3ebb310d3b9db675318a41f2115e77fba98e4a
Joe Cartonia <cssjoe@users.noreply.github.com>
2022-07-14 10:55:05 -0400

    Update PgCache_Flush.php
    Formatting: PHPCS WPCS

--- PgCache_Flush.php
+++ PgCache_Flush.php
@@ -76,7 +76,7 @@ class PgCache_Flush extends PgCache_ContentGrabber {
 		// calculate urls to purge
 		$full_urls = array();
 		$post = get_post( $post_id );
-		if (empty($post)) {
+		if ( empty( $post ) ) {
 			return true;
 		}
 		$is_cpt = Util_Environment::is_custom_post_type( $post );
