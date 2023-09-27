5eeb52cddf1f42414acfa2aa4b4ea8131e078b9f
Joe Cartonia <joec@boldgrid.com>
2023-09-27 16:07:03 -0400

    Remove unused widget data code

--- Cdn_BunnyCdn_Widget.php
+++ Cdn_BunnyCdn_Widget.php
@@ -25,7 +25,6 @@ class Cdn_BunnyCdn_Widget {
 		$o = new Cdn_BunnyCdn_Widget();
 
 		add_action( 'admin_print_styles', array( $o, 'admin_print_styles' ) );
-		add_action( 'admin_print_scripts', array( $o, 'admin_print_scripts' ) );
 
 		Util_Widget::add2(
 			'w3tc_bunnycdn',
@@ -56,82 +55,6 @@ class Cdn_BunnyCdn_Widget {
 	}
 
 	/**
-	 * W3TC AJAX: Get data for the widget.
-	 *
-	 * @since X.X.X
-	 *
-	 * @return void
-	 */
-	public static function w3tc_ajax_cdn_bunnycdn_widgetdata() {
-		$c  = Dispatcher::config();
-		$cs = Dispatcher::config_state();
-
-		$api = new Cdn_BunnyCdn_Api(
-			array(
-				'client_id'     => $c->get_string( 'cdn.bunnycdn.client_id' ),
-				'client_secret' => $c->get_string( 'cdn.bunnycdn.client_secret' ),
-				'stack_id'      => $c->get_string( 'cdn.bunnycdn.stack_id' ),
-				'access_token'  => $cs->get_string( 'cdn.bunnycdn.access_token' ),
-			)
-		);
-
-		$stack_id = $c->get_string( 'cdn.bunnycdn.stack_id' );
-		$site_id  = $c->get_string( 'cdn.bunnycdn.site_id' );
-		$response = array();
-
-		try {
-			$r         = $api->site_metrics( $site_id, 7 );
-			$series    = $r['series'][0];
-			$keys      = $series['metrics'];
-			$key_count = count( $keys );
-			$stats     = array();
-
-			foreach ( $series['samples'] as $sample ) {
-				$row = array();
-
-				for ( $n = 0; $n < $key_count; $n++ ) {
-					$row[ $keys[ $n ] ] = $sample['values'][ $n ];
-				}
-
-				$stats[] = $row;
-			}
-
-			$total_mb = 0;
-			$total_requests = 0;
-			$chart_mb = array( array( 'Date', 'MB', 'Requests' ) );
-
-			$dd = new \DateTime();
-
-			foreach ( $stats as $r ) {
-				$total_mb += $r['xferUsedTotalMB'];
-				$total_requests += $r['requestsCountTotal'];
-				$d = $dd->setTimestamp( (int) $r['usageTime'] );
-				$chart_mb[] = array(
-					$d->format( 'M/d' ),
-					$r['xferUsedTotalMB'],
-					$r['requestsCountTotal'],
-				);
-			}
-
-			$response['summary_mb']       = sprintf( '%.2f MB', $total_mb );
-			$response['summary_requests'] = $total_requests;
-			$response['chart_mb']         = $chart_mb;
-
-			$response['url_manage'] = esc_url(
-				'https://@todo/'
-			);
-
-			$response['url_reports'] = esc_url(
-				'https://@todo/'
-			);
-		} catch ( \Exception $ex ) {
-			$response['error'] = $ex->getMessage();
-		}
-
-		echo json_encode( $response );
-	}
-
-	/**
 	 * Enqueue styles.
 	 *
 	 * @since X.X.X
@@ -145,25 +68,6 @@ class Cdn_BunnyCdn_Widget {
 			'w3tc-bunnycdn-widget',
 			plugins_url( 'Cdn_BunnyCdn_Widget_View.css', W3TC_FILE ),
 			array(),
-			W3TC_VERSION
-		);
-	}
-
-	/**
-	 * Enqueue scripts.
-	 *
-	 * @since X.X.X
-	 *
-	 * @return void
-	 */
-	public function admin_print_scripts() {
-		wp_enqueue_script( 'google-jsapi', 'https://www.google.com/jsapi' );
-		wp_enqueue_script( 'w3tc-metadata' );
-		wp_enqueue_script( 'w3tc-widget' );
-		wp_enqueue_script(
-			'w3tc-bunnycdn-widget',
-			plugins_url( 'Cdn_BunnyCdn_Widget_View.js', W3TC_FILE ),
-			array( 'google-jsapi' ),
 			W3TC_VERSION
 		);
 	}
