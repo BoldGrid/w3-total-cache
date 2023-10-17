b399d99d88257dfd693720b05e3378a4815d860f
Joe Cartonia <joec@boldgrid.com>
2023-10-17 14:31:57 -0400

    Fix fatal on global $wp_filesystem call

--- Generic_AdminActions_Default.php
+++ Generic_AdminActions_Default.php
@@ -558,7 +558,9 @@ class Generic_AdminActions_Default {
 	 *
 	 * @return bool
 	 */
-	function enable_cookie_domain() {
+	public function enable_cookie_domain() {
+		WP_Filesystem();
+
 		global $wp_filesystem;
 
 		$config_path = Util_Environment::wp_config_path();
