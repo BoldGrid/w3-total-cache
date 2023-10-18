b4e3188e27dd775d925f8089a704e3acd16c3ca9
Joe Cartonia <joec@boldgrid.com>
2023-10-18 12:57:24 -0400

    Adjust widget

--- Cdn_BunnyCdn_Widget_View_Authorized.php
+++ Cdn_BunnyCdn_Widget_View_Authorized.php
@@ -24,24 +24,24 @@ defined( 'W3TC' ) || die();
 			<ul class="w3tc_bunnycdn_ul">
 				<li><a class="button w3tc_bunnycdn_href_manage" href=""><?php esc_html_e( 'Manage', 'w3-total-cache' ); ?></a></li>
 				<li><a class="button w3tc_bunnycdn_href_reports" href=""><?php esc_html_e( 'Reports', 'w3-total-cache' ); ?></a></li>
-				<li><a class="button" href="<?php echo esc_url( wp_nonce_url( admin_url( 'admin.php?page=w3tc_cdn&amp;w3tc_cdn_purge' ) ) ); ?>" onclick="w3tc_popupadmin_bar( this.href ); return false;"><?php esc_html_e( 'Purge', 'w3-total-cache' ); ?></a></li>
+				<li><a class="button" href="<?php echo \esc_url( \wp_nonce_url( \admin_url( 'admin.php?page=w3tc_cdn&amp;w3tc_cdn_purge' ) ) ); ?>" onclick="w3tc_popupadmin_bar( this.href ); return false;"><?php \esc_html_e( 'Purge', 'w3-total-cache' ); ?></a></li>
 			</ul>
 		</div>
 		<div class="w3tc_bunnycdn_summary">
-			<h4 class="w3tc_bunnycdn_summary_h4"><?php esc_html_e( 'Report - 7 days', 'w3-total-cache' ); ?></h4>
+			<h4 class="w3tc_bunnycdn_summary_h4"><?php \esc_html_e( 'Report - 7 days', 'w3-total-cache' ); ?></h4>
 		</div>
 		<ul class="w3tc_bunnycdn_ul">
 			<li>
-				<span class="w3tc_bunnycdn_summary_col1"><?php esc_html_e( 'Transferred', 'w3-total-cache' ); ?>:</span>
+				<span class="w3tc_bunnycdn_summary_col1"><?php \esc_html_e( 'Transferred', 'w3-total-cache' ); ?>:</span>
 				<span class="w3tc_bunnycdn_summary_col2 w3tc_bunnycdn_summary_mb"></span>
 			</li>
 			<li>
-				<span class="w3tc_bunnycdn_summary_col1"><?php esc_html_e( 'Requests', 'w3-total-cache' ); ?>:</span>
+				<span class="w3tc_bunnycdn_summary_col1"><?php \esc_html_e( 'Requests', 'w3-total-cache' ); ?>:</span>
 				<span class="w3tc_bunnycdn_summary_col2 w3tc_bunnycdn_summary_requests"></span>
 			</li>
 		</ul>
 		<div class="w3tc_bunnycdn_chart charts w3tc_bunnycdn_area">
-			<h4 class="w3tc_bunnycdn_h4"><?php esc_html_e( 'Requests', 'w3-total-cache' ); ?></h4>
+			<h4 class="w3tc_bunnycdn_h4"><?php \esc_html_e( 'Requests', 'w3-total-cache' ); ?></h4>
 			<div id="chart_div" style="width: 320px; height: 220px;margin-left: auto ;  margin-right: auto ;"></div>
 		</div>
 	</div>
--- Cdn_BunnyCdn_Widget_View_Unauthorized.php
+++ Cdn_BunnyCdn_Widget_View_Unauthorized.php
@@ -18,10 +18,10 @@ defined( 'W3TC' ) || die();
 			<?php
 			w3tc_e(
 				'cdn.bunnycdn.widget.v2.no_cdn',
-				sprintf(
+				\sprintf(
 					// translators: 1 HTML acronym for Content Delivery Network (CDN).
-					__( 'W3 Total Cache has detected that you do not have a %1$s configured', 'w3-total-cache' ),
-					'<acronym title="' . __( 'Content Delivery Network', 'w3-total-cache' ) . '">' . __( 'CDN', 'w3-total-cache' ) . '</acronym>'
+					\__( 'W3 Total Cache has detected that you do not have a %1$s configured', 'w3-total-cache' ),
+					'<acronym title="' . \__( 'Content Delivery Network', 'w3-total-cache' ) . '">' . \__( 'CDN', 'w3-total-cache' ) . '</acronym>'
 				)
 			);
 			?>
@@ -32,26 +32,26 @@ defined( 'W3TC' ) || die();
 		<?php
 		w3tc_e(
 			'cdn.bunnycdn.widget.v2.header',
-			sprintf(
+			\sprintf(
 				// translators: 1 HTML acronym for Content Delivery Network (CDN).
-				__( 'Enhance your website performance by adding Bunny CDN\'s (%1$s) service to your site.', 'w3-total-cache' ),
-				'<acronym title="' . __( 'Content Delivery Network', 'w3-total-cache' ) . '">' . __( 'CDN', 'w3-total-cache' ) . '</acronym>'
+				\__( 'Enhance your website performance by adding Bunny CDN\'s (%1$s) service to your site.', 'w3-total-cache' ),
+				'<acronym title="' . \__( 'Content Delivery Network', 'w3-total-cache' ) . '">' . \__( 'CDN', 'w3-total-cache' ) . '</acronym>'
 			)
 		);
 		?>
 	</p>
-	<h4 class="w3tc_bunnycdn_signup_h4"><?php esc_html_e( 'New customer? Sign up now to speed up your site!', 'w3-total-cache' ); ?></h4>
+	<h4 class="w3tc_bunnycdn_signup_h4"><?php \esc_html_e( 'New customer? Sign up now to speed up your site!', 'w3-total-cache' ); ?></h4>
 
 	<p>
 		<?php
 		w3tc_e(
 			'cdn.bunnycdn.widget.v2.works_magically',
-			__( 'Bunny CDN works magically with W3 Total Cache to speed up your site around the world for as little as $1 per month.', 'w3-total-cache' )
+			\__( 'Bunny CDN works magically with W3 Total Cache to speed up your site around the world for as little as $1 per month.', 'w3-total-cache' )
 		);
 		?>
 	</p>
 	<a class="button-primary" href="<?php echo esc_url( W3TC_BUNNYCDN_SIGNUP_URL ); ?>" target="_blank">
-		<?php esc_html_e( 'Sign Up Now ', 'w3-total-cache' ); ?>
+		<?php \esc_html_e( 'Sign Up Now ', 'w3-total-cache' ); ?>
 	</a>
 	<p>
 		<h4 class="w3tc_bunnycdn_signup_h4"><?php esc_html_e( 'Current customers', 'w3-total-cache' ); ?></h4>
@@ -59,21 +59,21 @@ defined( 'W3TC' ) || die();
 			<?php
 			w3tc_e(
 				'cdn.bunnycdn.widget.v2.existing',
-				sprintf(
+				\sprintf(
 					// translators: 1 HTML acronym for Content Delivery Network (CDN).
-					__(
+					\__(
 						'If you\'re an existing Bunny CDN customer, enable %1$s and authorize. If you need help configuring your %1$s, we also offer Premium Services to assist you.',
 						'w3-total-cache'
 					),
-					'<acronym title="' . __( 'Content Delivery Network', 'w3-total-cache' ) . '">' . __( 'CDN', 'w3-total-cache' ) . '</acronym>'
+					'<acronym title="' . \__( 'Content Delivery Network', 'w3-total-cache' ) . '">' . \__( 'CDN', 'w3-total-cache' ) . '</acronym>'
 				)
 			);
 			?>
 		</p>
-		<a class="button-primary" href="<?php echo esc_url( wp_nonce_url( Util_Ui::admin_url( 'admin.php?page=w3tc_cdn' ), 'w3tc' ) ); ?>" target="_blank">
-			<?php esc_html_e( 'Authorize', 'w3-total-cache' ); ?>
+		<a class="button-primary" href="<?php echo \esc_url( \wp_nonce_url( Util_Ui::admin_url( 'admin.php?page=w3tc_cdn' ), 'w3tc' ) ); ?>">
+			<?php \esc_html_e( 'Authorize', 'w3-total-cache' ); ?>
 		</a>
-		<a class="button" href="<?php echo esc_url( wp_nonce_url( Util_Ui::admin_url( 'admin.php?page=w3tc_support' ), 'w3tc' ) ); ?>">
-			<?php esc_html_e( 'Premium Services', 'w3-total-cache' ); ?>
+		<a class="button" href="<?php echo \esc_url( \wp_nonce_url( Util_Ui::admin_url( 'admin.php?page=w3tc_support' ), 'w3tc' ) ); ?>">
+			<?php \esc_html_e( 'Premium Services', 'w3-total-cache' ); ?>
 		</a>
 </div>
