1ba450c01a90a0c20748bf70cfc8ffe5abb97ac2
jacobd91 <jacobd@inmotionhosting.com>
2024-02-05 09:54:33 -0500

    Added convesio and dreamhost to partners widget, and adjusted copy.

--- Generic_WidgetPartners_View.php
+++ Generic_WidgetPartners_View.php
@@ -15,7 +15,15 @@ if ( ! defined( 'W3TC' ) ) {
 		<span class="partner-logo"><img src="<?php echo esc_url( plugins_url( 'w3-total-cache/pub/img/a2.png' ) ); ?>" width="25"></span>
 		<a href="<?php esc_url( 'https://www.a2hosting.com/kb/installable-applications/optimization-and-configuration/wordpress2/optimizing-wordpress-with-w3-total-cache-and-gtmetrix/' ); ?>" target="_blank">A2 Hosting</a>
 	</li>
+	<li>
+		<span class="partner-logo"><img src="<?php echo esc_url( plugins_url( 'w3-total-cache/pub/img/convesio.png' ) ); ?>" width="25"></span>
+		<a href="<?php esc_url( 'https://convesio.com/features/the-ultimate-guide-to-setting-up-w3-total-cache-with-convesio/' ); ?>" target="_blank">Convesio</a>
+	</li>
+	<li>
+		<span class="partner-logo"><img src="<?php echo esc_url( plugins_url( 'w3-total-cache/pub/img/dreamhost.png' ) ); ?>" width="25"></span>
+		<a href="<?php esc_url( 'https://www.dreamhost.com/wordpress/' ); ?>" target="_blank">Dreamhost</a>
+	</li>
 </ul>
-<p><?php esc_html_e( 'Hosting providers offering', 'w3-total-cache' ); ?> W3 Total Cache <?php esc_html_e( 'setup guides can show in this list too. Email', 'w3-total-cache' ); ?> <a href="mailto: partners@boldgrid.com">partners@boldgrid.com</a>.</p>
-<hr>
 <p><?php esc_html_e( 'Shopping around for better hosting? Checkout our', 'w3-total-cache' ); ?> <a href="<?php esc_url( 'https://www.boldgrid.com/wordpress-hosting/' ); ?>" target="_blank"><?php esc_html_e( 'WordPress Hosting Recommendations', 'w3-total-cache' ); ?></a></p>
+<hr>
+<p id="partner-bottom"><a href="mailto: partners@boldgrid.com"><?php esc_html_e( 'Are you a host', 'w3-total-cache' ); ?>?</a></p>
