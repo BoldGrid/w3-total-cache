7897279fe227413759ac6e57569fe3caba4947d0
jacobd91 <jacobd@inmotionhosting.com>
2024-02-05 10:13:21 -0500

    Updated partners widget to use W3 API for redirecting guide URLs.

--- Generic_WidgetPartners_View.php
+++ Generic_WidgetPartners_View.php
@@ -9,19 +9,19 @@ if ( ! defined( 'W3TC' ) ) {
 <ul id="partner-list">
 	<li>
 		<span class="partner-logo"><img src="<?php echo esc_url( plugins_url( 'w3-total-cache/pub/img/imh.png' ) ); ?>" width="25"></span>
-		<a href="<?php esc_url( 'https://www.inmotionhosting.com/support/edu/wordpress/plugins/w3-total-cache/' ); ?>" target="_blank">InMotion Hosting</a>
+		<a href="<?php esc_url( W3TC_PARTNER_IMH ); ?>" target="_blank">InMotion Hosting</a>
 	</li>
 	<li>
 		<span class="partner-logo"><img src="<?php echo esc_url( plugins_url( 'w3-total-cache/pub/img/a2.png' ) ); ?>" width="25"></span>
-		<a href="<?php esc_url( 'https://www.a2hosting.com/kb/installable-applications/optimization-and-configuration/wordpress2/optimizing-wordpress-with-w3-total-cache-and-gtmetrix/' ); ?>" target="_blank">A2 Hosting</a>
+		<a href="<?php esc_url( W3TC_PARTNER_A2 ); ?>" target="_blank">A2 Hosting</a>
 	</li>
 	<li>
 		<span class="partner-logo"><img src="<?php echo esc_url( plugins_url( 'w3-total-cache/pub/img/convesio.png' ) ); ?>" width="25"></span>
-		<a href="<?php esc_url( 'https://convesio.com/features/the-ultimate-guide-to-setting-up-w3-total-cache-with-convesio/' ); ?>" target="_blank">Convesio</a>
+		<a href="<?php esc_url( W3TC_PARTNER_CONVESIO ); ?>" target="_blank">Convesio</a>
 	</li>
 	<li>
 		<span class="partner-logo"><img src="<?php echo esc_url( plugins_url( 'w3-total-cache/pub/img/dreamhost.png' ) ); ?>" width="25"></span>
-		<a href="<?php esc_url( 'https://www.dreamhost.com/wordpress/' ); ?>" target="_blank">Dreamhost</a>
+		<a href="<?php esc_url( W3TC_PARTNER_DREAMHOST ); ?>" target="_blank">Dreamhost</a>
 	</li>
 </ul>
 <p><?php esc_html_e( 'Shopping around for better hosting? Checkout our', 'w3-total-cache' ); ?> <a href="<?php esc_url( 'https://www.boldgrid.com/wordpress-hosting/' ); ?>" target="_blank"><?php esc_html_e( 'WordPress Hosting Recommendations', 'w3-total-cache' ); ?></a></p>
