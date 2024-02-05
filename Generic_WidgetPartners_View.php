3998f4edb21b42abf6e0b76e1707709978241e73
jacobd91 <jacobd@inmotionhosting.com>
2024-02-05 10:18:24 -0500

    Fixed partner link print issue.

--- Generic_WidgetPartners_View.php
+++ Generic_WidgetPartners_View.php
@@ -9,19 +9,19 @@ if ( ! defined( 'W3TC' ) ) {
 <ul id="partner-list">
 	<li>
 		<span class="partner-logo"><img src="<?php echo esc_url( plugins_url( 'w3-total-cache/pub/img/imh.png' ) ); ?>" width="25"></span>
-		<a href="<?php esc_url( W3TC_PARTNER_IMH ); ?>" target="_blank">InMotion Hosting</a>
+		<a href="<?php echo esc_url( W3TC_PARTNER_IMH ); ?>" target="_blank">InMotion Hosting</a>
 	</li>
 	<li>
 		<span class="partner-logo"><img src="<?php echo esc_url( plugins_url( 'w3-total-cache/pub/img/a2.png' ) ); ?>" width="25"></span>
-		<a href="<?php esc_url( W3TC_PARTNER_A2 ); ?>" target="_blank">A2 Hosting</a>
+		<a href="<?php echo esc_url( W3TC_PARTNER_A2 ); ?>" target="_blank">A2 Hosting</a>
 	</li>
 	<li>
 		<span class="partner-logo"><img src="<?php echo esc_url( plugins_url( 'w3-total-cache/pub/img/convesio.png' ) ); ?>" width="25"></span>
-		<a href="<?php esc_url( W3TC_PARTNER_CONVESIO ); ?>" target="_blank">Convesio</a>
+		<a href="<?php echo esc_url( W3TC_PARTNER_CONVESIO ); ?>" target="_blank">Convesio</a>
 	</li>
 	<li>
 		<span class="partner-logo"><img src="<?php echo esc_url( plugins_url( 'w3-total-cache/pub/img/dreamhost.png' ) ); ?>" width="25"></span>
-		<a href="<?php esc_url( W3TC_PARTNER_DREAMHOST ); ?>" target="_blank">Dreamhost</a>
+		<a href="<?php echo esc_url( W3TC_PARTNER_DREAMHOST ); ?>" target="_blank">Dreamhost</a>
 	</li>
 </ul>
 <p><?php esc_html_e( 'Shopping around for better hosting? Checkout our', 'w3-total-cache' ); ?> <a href="<?php esc_url( 'https://www.boldgrid.com/wordpress-hosting/' ); ?>" target="_blank"><?php esc_html_e( 'WordPress Hosting Recommendations', 'w3-total-cache' ); ?></a></p>
