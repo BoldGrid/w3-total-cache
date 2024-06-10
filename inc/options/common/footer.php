a971ae12e88594232fc42fac77402386b59dab82
jacobd91 <jacobd@inmotionhosting.com>
2024-06-10 17:40:40 -0400

    Added missing file doc block. Added missing eof line break. Fixed HTML markup.

--- inc/options/common/footer.php
+++ inc/options/common/footer.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * File: footer.php
+ *
+ * W3TC admin page footer.
+ *
+ * @package W3TC
+ */
+
 namespace W3TC;
 
 if ( ! defined( 'W3TC' ) ) {
@@ -100,26 +108,26 @@ do_action( 'w3tc-dashboard-footer' );
 			</div>
 		</div>
 		<div class="w3tc-footer-column-2">
-<div class="w3tc-footer-inner-column-50">
-			<h2><?php esc_html_e( 'Follow Us', 'w3-total-cache' ); ?></h2>
-			<a class="w3tc-footer-link" target="_blank" href="<?php echo esc_url( 'https://twitter.com/w3edge' ); ?>" alt="<?php esc_attr_e( 'W3 Edge', 'w3-total-cache' ); ?>">
-				<span class="dashicons dashicons-twitter"></span><?php esc_html_e( 'W3 Edge', 'w3-total-cache' ); ?>
-			</a>
-			<a class="w3tc-footer-link" target="_blank" href="<?php echo esc_url( 'https://twitter.com/boldgrid' ); ?>" alt="<?php esc_attr_e( 'BoldGrid', 'w3-total-cache' ); ?>">
-				<span class="dashicons dashicons-twitter"></span><?php esc_html_e( 'BoldGrid', 'w3-total-cache' ); ?>
-			</a>
-			<a class="w3tc-footer-link" target="_blank" href="<?php echo esc_url( 'https://www.facebook.com/boldgrid/' ); ?>" alt="<?php esc_attr_e( 'BoldGrid', 'w3-total-cache' ); ?>">
-				<span class="dashicons dashicons-facebook"></span><?php esc_html_e( 'BoldGrid', 'w3-total-cache' ); ?>
-			</a>
-		</div>
-		<div class="w3tc-footer-inner-column-50">
-			<h2><?php esc_html_e( 'Partners', 'w3-total-cache' ); ?></h2>
-			<a class="w3tc-footer-link" target="_blank" href="<?php echo esc_url( W3TC_BUNNYCDN_SIGNUP_URL ); ?>" alt="Bunny CDN">
-				<div class="w3tc-bunnycdn-logo"></div>
-			</a>
-		</div>
-<div class="clear"></div>
-<h2><?php esc_html_e( 'Join the Newsletter!', 'w3-total-cache' ); ?></h2>
+			<div class="w3tc-footer-inner-column-50">
+				<h2><?php esc_html_e( 'Follow Us', 'w3-total-cache' ); ?></h2>
+				<a class="w3tc-footer-link" target="_blank" href="<?php echo esc_url( 'https://twitter.com/w3edge' ); ?>" alt="<?php esc_attr_e( 'W3 Edge', 'w3-total-cache' ); ?>">
+					<span class="dashicons dashicons-twitter"></span><?php esc_html_e( 'W3 Edge', 'w3-total-cache' ); ?>
+				</a>
+				<a class="w3tc-footer-link" target="_blank" href="<?php echo esc_url( 'https://twitter.com/boldgrid' ); ?>" alt="<?php esc_attr_e( 'BoldGrid', 'w3-total-cache' ); ?>">
+					<span class="dashicons dashicons-twitter"></span><?php esc_html_e( 'BoldGrid', 'w3-total-cache' ); ?>
+				</a>
+				<a class="w3tc-footer-link" target="_blank" href="<?php echo esc_url( 'https://www.facebook.com/boldgrid/' ); ?>" alt="<?php esc_attr_e( 'BoldGrid', 'w3-total-cache' ); ?>">
+					<span class="dashicons dashicons-facebook"></span><?php esc_html_e( 'BoldGrid', 'w3-total-cache' ); ?>
+				</a>
+			</div>
+			<div class="w3tc-footer-inner-column-50">
+				<h2><?php esc_html_e( 'Partners', 'w3-total-cache' ); ?></h2>
+				<a class="w3tc-footer-link" target="_blank" href="<?php echo esc_url( W3TC_BUNNYCDN_SIGNUP_URL ); ?>" alt="Bunny CDN">
+					<div class="w3tc-bunnycdn-logo"></div>
+				</a>
+			</div>
+			<div class="clear"></div>
+			<h2><?php esc_html_e( 'Join the Newsletter!', 'w3-total-cache' ); ?></h2>
 			<a>
 				<div id="mc_embed_shell">
 					<link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
@@ -136,11 +144,15 @@ do_action( 'w3tc-dashboard-footer' );
 										<div class="response" id="mce-error-response" style="display: none;"></div>
 										<div class="response" id="mce-success-response" style="display: none;"></div>
 									</div>
-								<div aria-hidden="true" style="position: absolute; left: -5000px;"><input type="text" name="b_a45a1f769145bb5516d43de7a_90e40613ad" tabindex="-1" value=""></div><div class="clear"><input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe" style="font-size: 13px;  background: #135e96;  border-color: #135e96;  color: #fff;"></div>
+								<div aria-hidden="true" style="position: absolute; left: -5000px;"><input type="text" name="b_a45a1f769145bb5516d43de7a_90e40613ad" tabindex="-1" value=""></div>
+								<div class="clear">
+									<input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe" style="font-size: 13px;  background: #135e96;  border-color: #135e96;  color: #fff;">
+								</div>
 							</div>
 						</form>
 					</div>
-				<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script></div>
+					<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+				</div>
 			</a>
 		</div>
 	</div>
