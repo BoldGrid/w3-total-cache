8d819b07725049bfa8e77e2542da593fd3a6b8a2
jacobd91 <jacobd@inmotionhosting.com>
2025-01-13 12:23:34 -0500

    Updated BunnyCDN unauthorized widget to display a notice recommending BunnyCDN if another CDN is configured.

--- Cdn_BunnyCdn_Widget_View_Unauthorized.php
+++ Cdn_BunnyCdn_Widget_View_Unauthorized.php
@@ -3,6 +3,7 @@
  * File: Cdn_BunnyCdn_Widget_View_Unauthorized.php
  *
  * @since   2.6.0
+ *
  * @package W3TC
  */
 
@@ -13,7 +14,105 @@ defined( 'W3TC' ) || die();
 ?>
 
 <div id="bunnycdn-widget" class="w3tc_bunnycdn_signup">
-	<?php if ( ! $c->get_boolean( 'cdn.enabled' ) ) : ?>
+	<?php
+	$cdn_engine = $c->get_string( 'cdn.engine' );
+	$cdn_name   = '';
+	switch ( true ) {
+		case ( 'ftp' === $cdn_engine ):
+			$cdn_name = 'Self-hosted / File Transfer Protocol Upload';
+			break;
+
+		case ( 's3' === $cdn_engine ):
+			$cdn_name = 'Amazon Simple Storage Service (S3)';
+			break;
+
+		case ( 's3_compatible' === $cdn_engine ):
+			$cdn_name = 'Amazon Simple Storage Service (S3) Compatible';
+			break;
+
+		case ( 'cf' === $cdn_engine ):
+			$cdn_name = 'Amazon CloudFront over S3';
+			break;
+
+		case ( 'cf2' === $cdn_engine ):
+			$cdn_name = 'Amazon CloudFront';
+			break;
+
+		case ( 'rscf' === $cdn_engine ):
+			$cdn_name = 'Rackspace Cloud Files';
+			break;
+
+		case ( 'azure' === $cdn_engine ):
+			$cdn_name = 'Microsoft Azure Storage';
+			break;
+
+		case ( 'azuremi' === $cdn_engine ):
+			$cdn_name = 'Microsoft Azure Storage (Managed Identity)';
+			break;
+
+		case ( 'google_drive' === $cdn_engine ):
+			$cdn_name = 'Google Drive';
+			break;
+
+		case ( 'mirror' === $cdn_engine ):
+			$cdn_name = 'Generic Mirror';
+			break;
+
+		case ( 'cotendo' === $cdn_engine ):
+			$cdn_name = 'Cotendo (Akamai)';
+			break;
+
+		case ( 'edgecast' === $cdn_engine ):
+			$cdn_name = 'Verizon Digital Media Services (EdgeCast) / Media Temple ProCDN';
+			break;
+
+		case ( 'att' === $cdn_engine ):
+			$cdn_name = 'AT&T';
+			break;
+
+		case ( 'akamai' === $cdn_engine ):
+			$cdn_name = 'Akamai';
+			break;
+
+		case ( 'highwinds' === $cdn_engine ):
+			$cdn_name = 'Highwinds';
+			break;
+
+		case ( 'limelight' === $cdn_engine ):
+			$cdn_name = 'LimeLight';
+			break;
+
+		case ( 'rackspace_cdn' === $cdn_engine ):
+			$cdn_name = 'RackSpace';
+			break;
+
+		case ( 'stackpath' === $cdn_engine ):
+			$cdn_name = 'StackPath SecureCDN (Legacy)';
+			break;
+
+		case ( 'stackpath2' === $cdn_engine ):
+			$cdn_name = 'StackPath';
+			break;
+	}
+
+	if ( ! empty( $cdn_name ) ) {
+		?>
+		<p class="notice notice-error">
+			<?php
+			w3tc_e(
+				'cdn.bunnycdn.widget.v2.no_cdn',
+				\sprintf(
+					// translators: 1 configured CDN name, 2 HTML acronym for Content Delivery Network (CDN).
+					\__( 'W3 Total Cache has detected that you have the %1$s %2$s configured which is fine, but we recommend using BunnyCDN instead.', 'w3-total-cache' ),
+					$cdn_name,
+					'<acronym title="' . \__( 'Content Delivery Network', 'w3-total-cache' ) . '">' . \__( 'CDN', 'w3-total-cache' ) . '</acronym>'
+				)
+			);
+			?>
+		</p>
+		<?php
+	} else {
+		?>
 		<p class="notice notice-error">
 			<?php
 			w3tc_e(
@@ -26,8 +125,10 @@ defined( 'W3TC' ) || die();
 			);
 			?>
 		</p>
-	<?php endif ?>
+		<?php
+	}
 
+	?>
 	<p>
 		<?php
 		w3tc_e(
