c63c49654e0e3595797ac7a17ee7048b6977958a
jacobd91 <jacobd@inmotionhosting.com>
2025-01-31 14:33:29 -0500

    Added check for license key for plugin data delete.

--- Root_Environment.php
+++ Root_Environment.php
@@ -221,7 +221,9 @@ class Root_Environment {
 		global $wpdb;
 
 		$license_key = $config->get_string( 'plugin.license_key' );
-		Licensing_Core::deactivate_license( $license_key );
+		if ( ! emtpy( $license_key ) ) {
+			Licensing_Core::deactivate_license( $license_key );
+		}
 
 		// Define prefixes for options and transients.
 		$prefixes = array(
