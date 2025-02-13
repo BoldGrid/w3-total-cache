f4927dbeca84e280253afbfba3bbf07a8e6830fc
jacobd91 <jacobd@inmotionhosting.com>
2025-02-13 10:04:15 -0500

    Added email field to exit survey.

--- inc/lightbox/exit_survey.php
+++ inc/lightbox/exit_survey.php
@@ -45,6 +45,13 @@
 				<input type="text" id="w3tc_exit_survey_uninstall_reason_other" class="hidden" name="other" placeholder="<?php esc_attr_e( 'Please specify...', 'w3-total-cache' ); ?>" />
 			</div>
 
+			<h2><?php esc_html_e( 'May we contact you?', 'w3-total-cache' ); ?></h2>
+			<p><?php esc_html_e( 'We\'re serious about making W3 Total Cache better, and sometimes that means reaching out to users like you to get a few more details about your experience. Would you be open to us contacting you for further feedback? If so, please enter your email address below.', 'w3-total-cache' ); ?></p>
+
+			<div class="w3tc-exit-survey-email">
+				<input id="email" type="email" id="w3tc_exit_survey_uninstall_reason_other" name="email" placeholder="<?php esc_attr_e( 'Email address...', 'w3-total-cache' ); ?>" />
+			</div>
+
 			<h2><?php esc_html_e( 'Remove all plugin data?', 'w3-total-cache' ); ?></h2>
 			<p><?php esc_html_e( 'Selecting "Yes" will permanently delete all W3 Total Cache settings, cached data, and other plugin-related information from your site. This action cannot be undone.', 'w3-total-cache' ); ?></p>
 
