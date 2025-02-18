f83b24ee191f822c065efe5189fc2d71a36a9743
Joe Cartonia <cssjoe@users.noreply.github.com>
2025-02-17 20:03:17 -0500

    Add phpunit tests (#1058)
    * Fix test class name
    * Fix delete W3TC options
    * Add delete_plugin_data test

--- Root_Environment.php
+++ Root_Environment.php
@@ -235,12 +235,12 @@ class Root_Environment {
 
 		// Delete options and transients with defined prefixes.
 		foreach ( $prefixes as $prefix ) {
-			$wpdb->query(
-				$wpdb->prepare(
-					"DELETE FROM {$wpdb->options} WHERE option_name LIKE %s",
-					$prefix . '%'
-				)
-			);
+			$query        = 'SELECT `option_name` FROM ' . $wpdb->options . ' WHERE `option_name` LIKE "' . $prefix . '%";';
+			$option_names = $wpdb->get_col( $query ); // phpcs:ignore WordPress.DB.PreparedSQL
+
+			foreach ( $option_names as $option_name ) {
+				delete_option( $option_name );
+			}
 		}
 
 		// Remove plugin-created directories.
