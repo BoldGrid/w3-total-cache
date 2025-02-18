27e7873da3f36c0e11d98abe900397a4a7cdc0d8
Joe Cartonia <joec@boldgrid.com>
2025-02-18 14:36:36 -0500

    Purge Object Cache files if disabled or engine changed

--- Generic_Plugin.php
+++ Generic_Plugin.php
@@ -835,14 +835,17 @@ class Generic_Plugin {
 
 			// Check if W3TC was updated to 2.8.6 or higher.
 			if ( \version_compare( W3TC_VERSION, '2.8.6', '>=' ) && ! in_array( '2.8.6', $ran_versions, true ) ) {
-				// Disable Object Cache if using Disk and show a notice in wp-admin.
+				// Disable Object Cache if using Disk, purge the cache files, and show a notice in wp-admin.
 				if ( $this->_config->get_boolean( 'objectcache.enabled' ) && 'file' === $this->_config->get_string( 'objectcache.engine' ) ) {
 					$this->_config->set( 'objectcache.enabled', false );
 					$this->_config->save();
-				}
 
-				// Set the flag to show the notice.
-				$state->set( 'tasks.notices.disabled_objdisk', true );
+					// Purge the Object Cache files.
+					Util_File::rmdir( Util_Environment::cache_blog_dir( 'object' ) );
+
+					// Set the flag to show the notice.
+					$state->set( 'tasks.notices.disabled_objdisk', true );
+				}
 
 				// Mark the task as ran.
 				$ran_versions[] = '2.8.6';
