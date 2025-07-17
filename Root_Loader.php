160e0c7955e0124db773d6b93fd1821200ea070d
jamesros <jamesros@inmotionhosting.com>
2025-07-17 13:30:10 -0400

    Add CDN Site Health test

--- Root_Loader.php
+++ Root_Loader.php
@@ -113,6 +113,7 @@ class Root_Loader {
 			$plugins[] = new UsageStatistics_Plugin_Admin();
 			$plugins[] = new SetupGuide_Plugin_Admin();
 			$plugins[] = new FeatureShowcase_Plugin_Admin();
+			$plugins[] = new Cdn_SiteHealth();
 		} elseif ( $c->get_boolean( 'jquerymigrate.disabled' ) ) {
 			$plugins[] = new UserExperience_Plugin_Jquery();
 		}
