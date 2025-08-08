cce3814dc5c6a6b57976f0d1fe87c723887c7095
jamesros <jamesros@inmotionhosting.com>
2025-08-08 14:06:20 -0400

    Enhance AI Crawler info report layout

--- Extension_AiCrawler_Page_View_Information.php
+++ Extension_AiCrawler_Page_View_Information.php
@@ -83,24 +83,54 @@ $report = array();
 if ( $response['success'] && isset( $response['data']['report'] ) ) {
         $report = $response['data']['report'];
 }
-?>
-<div class="metabox-holder">
-        <?php Util_Ui::postbox_header( esc_html__( 'Information', 'w3-total-cache' ), '', 'information' ); ?>
+
+/**
+ * Render a report block.
+ *
+ * @param string $title  Report heading.
+ * @param array  $report Report data keyed by URL.
+ *
+ * @return void
+ */
+function w3tc_aicrawler_render_report( $title, $report ) {
+        if ( empty( $report ) ) {
+                return;
+        }
+        ?>
+        <h4 class="w3tc-aicrawler-report-heading"><?php echo esc_html( $title ); ?></h4>
         <div class="w3tc-aicrawler-report">
                 <?php foreach ( $report as $url => $data ) :
                         $file       = basename( parse_url( $url, PHP_URL_PATH ) );
                         $present    = ! empty( $data['present'] );
                         $sufficient = ! empty( $data['sufficient'] );
                         $color      = $present && $sufficient ? 'green' : ( $present ? 'yellow' : 'red' );
+                        $icon       = $present && $sufficient ? '&#10003;' : ( $present ? '!' : '&#10005;' );
                         ?>
                         <div class="w3tc-aicrawler-report-item">
                                 <div class="w3tc-aicrawler-report-label"><?php echo esc_html( $file ); ?></div>
-                                <div class="w3tc-aicrawler-report-circle w3tc-aicrawler-report-circle-<?php echo esc_attr( $color ); ?>"></div>
+                                <div class="w3tc-aicrawler-report-circle w3tc-aicrawler-report-circle-<?php echo esc_attr( $color ); ?>"><?php echo $icon; ?></div>
                                 <?php if ( 'green' !== $color && ! empty( $data['evaluation'] ) ) : ?>
                                         <div class="w3tc-aicrawler-report-eval"><?php echo esc_html( $data['evaluation'] ); ?></div>
                                 <?php endif; ?>
                         </div>
                 <?php endforeach; ?>
         </div>
+        <?php
+}
+?>
+<div class="metabox-holder">
+        <?php Util_Ui::postbox_header( esc_html__( 'Information', 'w3-total-cache' ), '', 'information' ); ?>
+        <?php
+        $reports = array(
+                array(
+                        'title' => __( 'AI Crawlability Summary', 'w3-total-cache' ),
+                        'data'  => $report,
+                ),
+        );
+
+        foreach ( $reports as $r ) {
+                w3tc_aicrawler_render_report( $r['title'], $r['data'] );
+        }
+        ?>
         <?php Util_Ui::postbox_footer(); ?>
 </div>
